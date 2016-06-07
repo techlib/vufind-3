b80201248a93b3fbabbf2f6f989c33d7d602b188
Daniel Mareček <daniel.marecek@techlib.cz>
2016-06-07 11:54:04 +0200

    New version of js script + ntk changes. Manage location links + change element.

--- themes/ntk/js/check_item_statuses.js
+++ themes/ntk/js/check_item_statuses.js
@@ -1,73 +1,97 @@
-/*global path*/
+/*global VuFind */
 
-function checkItemStatuses() {
-  var id = $.map($('.ajaxItem'), function(i) {
-    return $(i).find('.hiddenId')[0].value;
+function checkItemStatuses(container) {
+  if (typeof(container) == 'undefined') {
+    container = $('body');
+  }
+
+  var elements = {};
+  var data = $.map(container.find('.result'), function(record) { // zmena prvku, kvuli zmene css pozicovani
+    if ($(record).find('.hiddenId').length == 0) {
+      return null;
+    }
+    var datum = $(record).find('.hiddenId').val();
+    if (typeof elements[datum] === 'undefined') {
+        elements[datum] = $();
+    }
+    elements[datum] = elements[datum].add($(record));
+    return datum;
   });
-  if (!id.length) {
+  if (!data.length) {
     return;
   }
 
   $(".ajax-availability").removeClass('hidden');
   $.ajax({
     dataType: 'json',
-    url: path + '/AJAX/JSON?method=getItemStatuses',
-    data: {id:id},
-    success: function(response) {
-      if(response.status == 'OK') {
-        $.each(response.data, function(i, result) {
-          var item = $($('.result')[result.record_number]); // DM - zmena prvku, kvuli zmene css pozicovani
-
-          item.find('.status').empty().append(result.availability_message);
-          if (typeof(result.full_status) != 'undefined'
-            && result.full_status.length > 0
-            && item.find('.callnumAndLocation').length > 0
+    method: 'POST',
+    url: VuFind.path + '/AJAX/JSON?method=getItemStatuses',
+    data: {'id':data}
+  })
+  .done(function(response) {
+    $.each(response.data, function(i, result) {
+      var item = elements[result.id];
+      if (!item) {
+        return;
+      }
+      
+      item.find('.status').empty().append(result.availability_message);
+      if (typeof(result.full_status) != 'undefined'
+        && result.full_status.length > 0
+        && item.find('.callnumAndLocation').length > 0
+      ) {
+        // Full status mode is on -- display the HTML and hide extraneous junk:
+        item.find('.callnumAndLocation').empty().append(result.full_status);
+        item.find('.callnumber').addClass('hidden');
+        item.find('.location').addClass('hidden');
+        item.find('.hideIfDetailed').addClass('hidden');
+        item.find('.status').addClass('hidden');
+      } else if (typeof(result.missing_data) != 'undefined'
+        && result.missing_data
+      ) {
+        // No data is available -- hide the entire status area:
+        item.find('.callnumAndLocation').addClass('hidden');
+        item.find('.status').addClass('hidden');
+      } else if (result.locationList) {
+        // We have multiple locations -- build appropriate HTML and hide unwanted labels:
+        item.find('.callnumber').addClass('hidden');
+        item.find('.hideIfDetailed').addClass('hidden');
+        item.find('.location').addClass('hidden');
+        var locationListHTML = "";
+        for (var x=0; x<result.locationList.length; x++) {
+          locationListHTML += '<div class="groupLocation">';
+          if (result.locationList[x].availability) {
+            locationListHTML += '<i class="fa fa-ok text-success"></i> <span class="text-success">'
+              + result.locationList[x].location + '</span> ';
+          } else if (typeof(result.locationList[x].status_unknown) !== 'undefined'
+              && result.locationList[x].status_unknown
           ) {
-            // Full status mode is on -- display the HTML and hide extraneous junk:
-            item.find('.callnumAndLocation').empty().append(result.full_status);
-            item.find('.callnumber').addClass('hidden');
-            item.find('.location').addClass('hidden');
-            item.find('.hideIfDetailed').addClass('hidden');
-            item.find('.status').addClass('hidden');
-          } else if (typeof(result.missing_data) != 'undefined'
-            && result.missing_data
-          ) {
-            // No data is available -- hide the entire status area:
-            item.find('.callnumAndLocation').addClass('hidden');
-            item.find('.status').addClass('hidden');
-          } else if (result.locationList) {
-            // We have multiple locations -- build appropriate HTML and hide unwanted labels:
-            item.find('.callnumber').addClass('hidden');
-            item.find('.hideIfDetailed').addClass('hidden');
-            item.find('.location').addClass('hidden');
-            var locationListHTML = "";
-            for (var x=0; x<result.locationList.length; x++) {
-              locationListHTML += '<div class="groupLocation">';
-              if (result.locationList[x].availability) {
-                locationListHTML += '<i class="fa fa-ok text-success"></i> <span class="text-success">'
-                  + result.locationList[x].location + '</span> ';
-              } else {
-                locationListHTML += '<i class="fa fa-remove text-error"></i> <span class="text-error"">'
-                  + result.locationList[x].location + '</span> ';
-              }
-              locationListHTML += '</div>';
-              locationListHTML += '<div class="groupCallnumber">';
-              locationListHTML += (result.locationList[x].callnumbers)
-                   ?  result.locationList[x].callnumbers : '';
-              locationListHTML += '</div>';
+            if (result.locationList[x].location) {
+              locationListHTML += '<i class="fa fa-status-unknown text-warning"></i> <span class="text-warning">'
+                + result.locationList[x].location + '</span> ';
             }
-            item.find('.locationDetails').removeClass('hidden');
-            item.find('.locationDetails').empty().append(locationListHTML);
           } else {
-            // Default case -- load call number and location into appropriate containers:
-            item.find('.callnumber').empty().append(result.callnumber+'<br/>');
-            item.find('.location').empty().append(
-              result.reserve == 'true'
-              ? result.reserve_message
-              : result.location
-            );
-
-            // links
+            locationListHTML += '<i class="fa fa-remove text-danger"></i> <span class="text-danger"">'
+              + result.locationList[x].location + '</span> ';
+          }
+          locationListHTML += '</div>';
+          locationListHTML += '<div class="groupCallnumber">';
+          locationListHTML += (result.locationList[x].callnumbers)
+               ?  result.locationList[x].callnumbers : '';
+          locationListHTML += '</div>';
+        }
+        item.find('.locationDetails').removeClass('hidden');
+        item.find('.locationDetails').empty().append(locationListHTML);
+      } else {
+        // Default case -- load call number and location into appropriate containers:
+        item.find('.callnumber').empty().append(result.callnumber+'<br/>');
+        item.find('.location').empty().append(
+          result.reserve == 'true'
+          ? result.reserve_message
+          : result.location
+        );
+        
+        // links
             if (result.location == "V&Scaron;CHT &uacute;stavy"){
                 item.find('.location').empty().append("<a href='https://www.chemtk.cz/cs/82950-seznam-ustavnich-knihoven'>"+result.location+"</a>");
             } else if (result.location == "UCT departments"){
@@ -90,12 +114,12 @@ function checkItemStatuses() {
                     (result.location == "&Uacute;OCHB &uacute;stav") || (result.location == "IOCB department") ||
                     (result.location == "Book news, 4th floor") || (result.location == "Novinky, 4. NP")
                     ){
-	            item.find('.location').empty().append(result.location);
+                    item.find('.location').empty().append(result.location);
             } else {
                 item.find('.location').empty().append("<a href=''>"+result.location+"</a>");
                 item.find('.location').click(function() {
 
-                    var title = result.location; 
+                    var title = result.location;
                     if(typeof title === "undefined") {
                         title = $(this).html();
                     }
@@ -127,18 +151,19 @@ function checkItemStatuses() {
                     $('#modal .modal-title').html(vysledek);
                     Lightbox.titleSet = true;
 
-			        return Lightbox.get('map','lcc',result.callnumber);
-		        });
+                                return Lightbox.get('map','lcc',result.callnumber);
+                        });
             }
-          }
-
-        });
-      } else {
-        // display the error message on each of the ajax status place holder
-        $(".ajax-availability").empty().append(response.data);
       }
-      $(".ajax-availability").removeClass('ajax-availability');
-    }
+    });
+
+    $(".ajax-availability").removeClass('ajax-availability');
+  })
+  .fail(function(response, textStatus) {
+    $('.ajax-availability').empty();
+    if (textStatus == 'abort' || typeof response.responseJSON === 'undefined') { return; }
+    // display the error message on each of the ajax status place holder
+    $('.ajax-availability').append(response.responseJSON.data).addClass('text-danger');
   });
 }
 
