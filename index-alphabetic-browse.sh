ca92130ba9deea8790f560443b9c5e3cd98174ab
Daniel Mareček <daniel.marecek@techlib.cz>
2017-03-24 17:10:25 +0100

    Repair settings of alphabetical browse. Schema, import, index.

--- index-alphabetic-browse.sh
+++ index-alphabetic-browse.sh
@@ -75,8 +75,8 @@ function build_browse
 }
 build_browse "hierarchy" "hierarchy_browse"
 build_browse "title" "title_fullStr" 1 "-Dbibleech=StoredFieldLeech -Dsortfield=title_sort -Dvaluefield=title_fullStr"
-#build_browse "topic" "topic_browse"
-build_browse "topic" "topic_facet" # zmena z topic_browse na topic_facet, v puvodnim poli jsou zaindexovana i nezadouci podpole
+build_browse "topic" "topic_browse"
+#build_browse "topic" "topic_facet" zmena z topic_browse na topic_facet, v puvodnim poli jsou zaindexovana i nezadouci podpole
 build_browse "author" "author_browse"
 build_browse "lcc" "callnumber-raw" 1 "-Dbrowse.normalizer=org.vufind.util.LCCallNormalizer"
 build_browse "dewey" "dewey-raw" 1 "-Dbrowse.normalizer=org.vufind.util.DeweyCallNormalizer"
